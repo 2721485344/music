e470a91e26f0c5d02fdf83201486a02dc197581e
zhangxiaolei0123 <zhangxiaolei0123@users.noreply.github.com>
2018-09-04 13:26:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import numpy as np
 import tensorflow as tf
 from  flask import Flask ,jsonify,render_template,request
 
-from 深度学习.手写数字.mnist import model
+from 深度学习.mnist import model
 x=tf.placeholder("float",[None,784])
 sess=tf.Session()
 
 with tf.variable_scope("regression"):
     y1,variables=model.regression(x)
 saver=tf.train.Saver(variables)
-saver.restore(sess,'深度学习/手写数字/mnist/data/regression.ckpt')
+saver.restore(sess,'深度学习/mnist/data/regression.ckpt')
 
 with tf.variable_scope("convolutional"):
     keep_prob=tf.placeholder('float')
     y2,variables=model.convolutional(x,keep_prob)
 saver=tf.train.Saver(variables)
-saver.restore(sess,'深度学习/手写数字/mnist/data/regression.ckpt')
+saver.restore(sess,'深度学习/mnist/data/regression.ckpt')
 
 def regression(input):
     return  sess.run(y1,feed_dict={x:input}).flatten().tolist()
